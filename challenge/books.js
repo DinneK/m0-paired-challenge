cb646e8515e8a3594d5f7efd755700fab568f417
Dinne Kopelevich <dinne22@hotmail.com>
2022-04-19 09:53:24 -0600

    Add books and markdown

---
+++ challenge/books.js
@@ -0,0 +1,14 @@
+// This variable is for the author.
+var author = "Nicolas Sparks";
+// The number of books the author has written.
+var numBook = 5;
+// This variable is for the author's most popular book.
+var title = "The Notebook";
+// Genre is the type if subject the other writes.
+var genre = "Romance";
+var bookStore = 1000;
+var softCover = true;
+
+//These are titles of books and arrays are multiple elements, therefore the variable name
+//is plural.
+var bookTitles = ["The Notebook", "Never Let Me Go", "Free Food For Millionaires"];
---
+++ challenge/index.md
@@ -0,0 +1,13 @@
+# Book Titles
+
+1. To Kill a Mocking Bird
+2. Never Let Me Go
+3. Pachinko
+4. The Brass City
+5. Little Fires Everywhere
+
+- In this challenge we decided to work through the problems together, each of us working
+in our own terminal. I think we collaborated well, although nerves took over for me.
+I didn't even think to offer to go over the directions first, because I felt like we
+were so crunched for time, although in the future, I think that should be the first step.
+We were very good at talking through each step.
